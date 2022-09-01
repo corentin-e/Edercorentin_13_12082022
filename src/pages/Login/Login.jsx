26258a60fb4162adb8287da0303829d013f1c011
Etha <76168237+etharion29@users.noreply.github.com>
2022-09-01 20:51:59 +0200

    Little modification

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,14 +10,14 @@ const Login = () => {
                 <label className='login-card_title'> Sign In</label>
                 <div className="login-card_form">
                     <label>Username</label>
-                    <input className='login-form_input'></input>
+                    <input type="email" name="username" id="username" className='login-form_input'/>
                 </div>
                 <div className="login-card_form">
                     <label>Password</label>
-                    <input className='login-form_input'></input>
+                    <input type="password" name="password" id="password" className='login-form_input'/>
                 </div>
                 <div className='login-card_option'>
-                    <input type="checkbox" className='login-option_checkbox'></input>
+                    <input type="checkbox" className='login-option_checkbox'/>
                     <label>Remember me</label>
                 </div>
                 <button type="submit" className='login-button'>Sign in</button>
