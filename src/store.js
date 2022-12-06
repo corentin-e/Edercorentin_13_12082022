acb5fefdbf5b6c24962cf1e3217293a9226b4c6f
Etha <76168237+etharion29@users.noreply.github.com>
2022-12-06 23:22:23 +0100

    fix little bug on the home page

--- src/store.js
+++ src/store.js
@@ -2,16 +2,6 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import authReducer from "./redux/authSlice";
 import userReducer from "./redux/userSlice";
 
-/* import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist'; */
-
-/* const persistConfig = {
-  key: 'root',
-  storage,
-} */
-
-/* const persistedReducer = persistReducer(persistConfig, authReducer, userReducer) */
-
 const reducer = combineReducers({
   auth: authReducer,
   user: userReducer,
@@ -20,5 +10,3 @@ const reducer = combineReducers({
 export default configureStore({
   reducer,
 })
-
-/* export const persistor = persistStore(configureStore) */
